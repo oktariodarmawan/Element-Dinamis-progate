649c7bfa02dac002e79e53afde3579994b29f962
oktariodarmawan <63888215+oktariodarmawan@users.noreply.github.com>
2023-08-18 05:12:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let slideIndex = 0;
+
+const updateSlide = (n) => {
+    slideIndex += n;
+    showSlide(slideIndex);
+  }
+
+
+const showSlide = (n)=>{
+    const slides = document.getElementsByClassName('contents-wrapper')
+    if(n > slides.length -1 ){
+        slideIndex = 0;
+    }
+
+    if(n < 0){
+        slideIndex = slides.length -1
+    }
+
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+    slides[slideIndex].style.display = "flex";
+}
+
+showSlide(slideIndex)
